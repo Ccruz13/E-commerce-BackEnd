777aa657ec49cd991e2da1782c3e4c0dcfda6f28
Ccruz13 <88520938+Ccruz13@users.noreply.github.com>
2021-11-20 22:16:37 -0500

    added tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,7 +27,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  Category.findAll({
+  Category.findOne({
     where: {
       id: req.params.id
     },
